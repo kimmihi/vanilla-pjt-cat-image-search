b1b451b16e49e57498cc1f679df302ad4a4adade
kimmeh <mhko0411@gmail.com>
2022-10-07 00:20:17 +0900

    feat: 최근 검색어 추가

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,13 +1,20 @@
 export class SearchInput {
   #text = "";
+  #recentKeywordList = [];
   constructor({ $target, onSearch }) {
     const searchBar = document.createElement("section");
     const searchInput = document.createElement("input");
     const submitBtn = document.createElement("button");
+    const recentKeywordContainer = document.createElement("section");
+
+    this.$target = $target;
+    this.onSearch = onSearch;
+    this.recentKeywordContainer = recentKeywordContainer;
 
     searchBar.className = "SearchBar";
     searchInput.className = "SearchInput";
     submitBtn.className = "SubmitBtn";
+    recentKeywordContainer.className = "RecentKeywordContainer";
 
     searchInput.autofocus = true;
     searchInput.placeholder = "고양이를 검색해보세요.";
@@ -17,12 +24,14 @@ export class SearchInput {
     searchBar.appendChild(submitBtn);
 
     $target.appendChild(searchBar);
+    $target.appendChild(this.recentKeywordContainer);
 
     searchInput.addEventListener("keyup", (e) => {
       this.setState(e.target.value);
       if (e.isComposing) return;
       if (e.key === "Enter") {
         onSearch(this.#text);
+        this.#addRecentKeyword(this.#text);
       }
     });
 
@@ -33,7 +42,46 @@ export class SearchInput {
     submitBtn.addEventListener("click", () => {
       if (this.#text.length === 0) return;
       onSearch(this.#text);
+      this.#addRecentKeyword(this.#text);
     });
+  }
+
+  #addRecentKeyword(keyword) {
+    const size = this.#recentKeywordList.length;
+
+    if (size >= 5) {
+      this.#recentKeywordList.pop();
+      this.#recentKeywordList.unshift(keyword);
+    } else {
+      this.#recentKeywordList.unshift(keyword);
+    }
+
+    this.#paintRecentKeyword();
+  }
+
+  #paintRecentKeyword() {
+    this.recentKeywordContainer.innerHTML = "";
+
+    const recentTitle = document.createElement("p");
+    const recentButtonGroup = document.createElement("section");
+    const frag = document.createDocumentFragment();
+
+    recentTitle.innerText = "최근 검색어";
+    recentButtonGroup.className = "ButtonGroup";
+
+    this.recentKeywordContainer.appendChild(recentTitle);
+    this.recentKeywordContainer.appendChild(recentButtonGroup);
+
+    this.#recentKeywordList.forEach((keyword) => {
+      const chip = document.createElement("button");
+      chip.innerText = keyword;
+      chip.addEventListener("click", () => {
+        this.onSearch(keyword);
+      });
+      frag.appendChild(chip);
+    });
+    recentButtonGroup.appendChild(frag);
+    this.recentKeywordContainer.appendChild(recentButtonGroup);
   }
 
   setState(newState) {
